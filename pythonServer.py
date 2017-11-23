2f4eb5a8db1c7fd6a3e90f84b8cad5a15bf22f06
bquoctruong <brianquochungtruong@gmail.com>
2017-11-22 16:25:47 -0800

    Update pythonServer.py

--- pythonServer.py
+++ pythonServer.py
@@ -1,5 +1,8 @@
 import os
 from socket import *
+import socket
+import threading
+import ftplib
 # Function:
 # Date of code (Last upInedt: 
 # Output: r: 
@@ -7,7 +10,7 @@ from socket import *
 # Input: 
 # Output: 
 #Obtains file from remote server
-def downloadFile():
+def downloadFileFTP():
     filename = 'fileName.txt'
     localfile = open(filename, 'wb')
     ftp.retrbinary('RETR ' + filename, localfile.write, 4096)
@@ -15,12 +18,12 @@ def downloadFile():
     localfile.close()
 
 #Places a file
-def uploadFile():
+def uploadFileFTP():
     filename = 'fileName.txt'
     ftp.storbinary('STOR ' + filename, open(filename, 'rb'))
     ftp.quit()
 
-def runServer():
+def runServerSocket():
     print("Running server")
 
     #Initiate port
@@ -43,6 +46,7 @@ def runServer():
     while(1):
         #Accept conncetion
         connectionSocket, addr = serverSocket.accept()
+        print("Connected to client")
 
         #Recieve whatever the newly connected client has to send
         temporaryBuffer = ""
@@ -65,4 +69,42 @@ def runServer():
     #Close socket
     connectionSocket.close()
 
-runServer()
+def RetrFile(name, sock):
+    filename = socket.recv(1024)
+    if os.path.isfile(filename):
+        sock.send("Exists " + str(os.path.getsize(filename)))
+        userResponse = sock.recv(1024)
+        if userResponse[:2] == 'OK':    #Check first two bytes to see if it's okay
+            with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)  #Read 1024 bytes from the file
+                sock.send(bytesToSend)
+                while bytesToSend != "":    #If file exceeds 1024 bytes, grab another 1024 bytes
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else:   #If file doesn't exist
+        sock.send("ERR")
+    sock.close()
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host,port)) #Bind socket to host and port
+
+    s.listen(5)
+
+    print("Server Started")
+    while True:
+        c, addr = s.accept()
+        print("Client connected to IP :< " + str(addr) + ">")
+        t = threading.Thread(target = RetrFile, args=("retrThread", c))
+        t.start()
+    s.close()
+
+if __name__ == '__main__':
+    Main()
+
+
+#Resources: https://www.youtube.com/watch?v=LJTaPaFGmM4
+
