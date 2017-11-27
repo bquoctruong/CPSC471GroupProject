d9f272215e2799944899e1d4aaee0892784372af
bquoctruong <brianquochungtruong@gmail.com>
2017-11-27 00:20:48 -0800

    Final Commit

--- pythonClient.py
+++ pythonClient.py
@@ -1,108 +1,114 @@
+#Name: Brian Truong
+#Date: 11/12/17
+#File Name: pythonClient.py
+#File Description:  Runs an FTP client to interact with pythonServer.py
+#NOTE: Done in Python 3.6
 from socket import *
 import socket
 import os
 import ftplib
 from ftplib import *
+from time import sleep
 
-#To-do: - Append size of file to the first 10 bytes of data (See example code)
- 
-
-# Function:
-# Date of code (Last upInedt: 
-# Output: r: 
-# Description: 
-# Input: 
-# Output: 
-#Obtains file from remote server
-def downloadFileFTP():
-    filename = 'fileName.txt'
-    localfile = open(filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, localfile.write, 4096)
-    ftp.quit()
-    localfile.close()
-
-#Places a file
-def uploadFileFTP():
-    filename = 'fileName.txt'
-    ftp.storbinary('STOR ' + filename, open(filename, 'rb'))
-    ftp.quit()
-
-def runClientSocket():
-    print("Running client")
-
-    #Get hostname and port
-    inputHost = str(input("Enter the host name: "))
-    inputPortNumber = int(input("Enter the port number: "))
-    bindHostNumber = (inputHost, inputPortNumber)
-
-    #Create socket
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-
-    #Connect to server
-    clientSocket.connect(bindHostNumber)
-    print("Connected to server")
-
-    #Data sent
-    file = open('text.txt', 'rb')
-    length = file.read(1024)
-
-    #Bytes
-    bytesSent = 0
-
-    #Send string
-    while bytesSent != len(file):
-        #Send data
-        bytesSent += clientSocket.send(data[bytesSent:])
-
-    #Close socket
-    print("Closing socket")
-    clientSocket.close()
-
-def runClientFTP():
-    ftp = FTP.connect(host='', port = 1234)
-    ftp.login()
-    while ftp:
-        print("Connected to server")
+# Function: Main
+# Date of code (Last updated): 11/25/17
+# Programmer: Brian Truong
+# Description: Runs the entirety of the ftp client
+# Input: N/A
+# Output: N/A
 
 def Main():
-    host = 'localhost'
-    port = 5000
 
+    #Prompts user to enter host and port number to connect to
+    host = input("Enter host name: ")
+    port = int(input("Enter port number: "))
+
+    #Establishes connection with given host and port number
     s = socket.socket()
     s.connect((host,port))
 
-    filename = input("Enter the file's name: ")
-    byteName = str.encode(filename)
-    if filename != 'q':
-        s.send(byteName)
-        #Wait to see if the file exists or not
-        data = s.recv(1024)     #Receive 1024 bytes
-        #if data[:6] == 'EXISTS':    #If the first 6 bytes are EXISTS
-        filesize = int(data[6:])
-        message = str(input("File Exists, " + str(filesize) + "Bytes, download? (Y/N) >"))
-        if message == 'Y':
-            okMessage = 'OK'
-            okMessage = str.encode(okMessage)
-            s.send(okMessage)
-            f = open('new_'+filename, 'wb')
-            data = s.recv(1024)
-            totalRecv = len(data)
-            f.write(data)
-            #if there is more than 1024 bytes, receive more
-            while totalRecv < filesize:
-                data = s.recv(1024)
-                totalRecv += len(data)
-                f.write(data)
-                print("{0:.2f}".format((totalRecv / float(filesize)) * 100) + "% Done")
-            print("Download Complete")
-    else:
+    #After connection is established, gives user a list of prompts
+    print("Commands:")
+    print("get <filename>")
+    print("put <filename>")
+    print("ls")
+    print("quit")
+
+    #User is prompted to enter a command
+    answer = input("ftp> ")
+
+    #If user enters get <filename>, client sends filename to server and command to "get" file
+    if(answer[:3] == "get"):
+        systemMessage = 'GET'   #Sends system message to prompt server to use correct function
+        systemMessage = str.encode(systemMessage)   #Encodes message for socket transmission
+        s.send(systemMessage)   #Sends message to server
+        filename = answer[4:]   #Obtains file name from user-inputted string
+        byteName = str.encode(filename)
+        if filename != 'q':
+            s.send(byteName)
+            #Wait to see if the file exists or not
+            data = s.recv(1024)     #Receive 1024 bytes
+            if (str(data)[2:8] == 'EXISTS'):    #If the first 6 bytes are EXISTS, continues on with retrieving file; first 2 bytes are encoders
+                filesize = str(data)[8:]    #Obtains file size
+                filesize = filesize[:-1]    #Cuts off "'" at the end; Credit: https://stackoverflow.com/questions/1798465/python-remove-last-3-characters-of-a-string
+                filesize = int(filesize)    #Converts filesize to int
+                message = str(input("File Exists, " + str(filesize) + " Bytes, download? (Y/N) >"))
+                if message == 'Y':
+                    okMessage = 'OK'    #Sends system message saying it is okay to send file
+                    okMessage = str.encode(okMessage)
+                    s.send(okMessage)
+                    f = open('new_'+filename, 'wb') #Opens local file in client's directory
+                    data = s.recv(1024) #Receives first 1024 bytes
+                    totalRecv = len(data)   #Tracks amount of data received
+                    while(data):
+                        f.write(data)   #Writes data received to file
+                        data = s.recv(1024) #Obtains more bytes from server
+                        totalRecv += len(data)  #Increments total bytes received
+                        print("{0:.2f}".format((totalRecv / float(filesize)) * 100) + "% Done") #Keeps progress of download to user
+                    print("Download Complete")
+                    s.close()
+            else:
+                    print("File does not exist")
+                    s.close()
+        else:
+            s.close()
+    #If put is selected, transfers a file from the client's directory to the server
+    if(answer[:3] == "put"):
+        systemMessage = 'PUT'   #Creates a system message to send to server
+        systemMessage = str.encode(systemMessage)
+        s.send(systemMessage)   #Sends message to server
+        filename = answer[4:]   #Obtains file name from user-inputted list
+        if os.path.isfile(filename):
+            print("Found file")
+            byteName = str.encode(filename)
+            s.send(byteName)
+            f = open(filename, 'rb')    #Opens file for reading
+            l = f.read(1024)    #Reads first 1024 bytes of file
+            filesize = os.path.getsize(filename)    #Obtains filesize of file
+            totalRecv = len(l)  #Keeps track of how much data is being sent
+            while(l):
+                s.send(l)
+                l = f.read(1024)
+                totalRecv += len(l)
+                print("{0:.2f}".format((totalRecv / float(filesize)) * 100) + "% Done") #Keeps progress of download to user
+        else:
             print("File does not exist")
-    s.close()
+        s.close()
+    #If ls is inputted, sends message to server to list all files in the directory
+    if(answer == "ls"):
+        systemMessage = 'LS'    #Creates message for server
+        systemMessage = str.encode(systemMessage)
+        s.send(systemMessage)   #Sends message to server
+        l = s.recv(1024)    #Obtains part of list from server
+        l = str(l)[2:-1]    #Trims beginning and end
+        while(l):
+            print(l)    #Prints whatever is on the server
+            l = s.recv(1024)    #Obtains more bytes
+            l = str(l)[2:-1]    #Trims beginning and end
+        s.close()
+#Initialize client
 if __name__=="__main__":
     Main()
-
-
-
 
 #Resources: https://stackoverflow.com/questions/27241804/sending-a-file-over-tcp-sockets-in-python
 #https://www.youtube.com/watch?v=LJTaPaFGmM4
--- pythonServer.py
+++ pythonServer.py
@@ -1,109 +1,98 @@
+#Name: Brian Truong
+#Date: 11/12/2017
+#File Name: pythonServer.py
+#File Description: Creates an FTP like server to run in conjunction with pythonClient.py
+#NOTE: Done in Python 3.6
 import os
 from socket import *
 import socket
 import threading
 import ftplib
-# Function:
-# Date of code (Last upInedt: 
-# Output: r: 
-# Description: 
-# Input: 
-# Output: 
-#Obtains file from remote server
-def downloadFileFTP():
-    filename = 'fileName.txt'
-    localfile = open(filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, localfile.write, 4096)
-    ftp.quit()
-    localfile.close()
 
-#Places a file
-def uploadFileFTP():
-    filename = 'fileName.txt'
-    ftp.storbinary('STOR ' + filename, open(filename, 'rb'))
-    ftp.quit()
-
-def runServerSocket():
-    print("Running server")
-
-    #Initiate port
-    inputPortNumber = int(input("Enter the port number: "))
-    inputHostName = ''
-    bindedHostNum = (inputHostName, inputPortNumber)
-    
-    #create TCP socket
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-
-    #Bind socket to port
-    serverSocket.bind(bindedHostNum)
-
-    #Listens for incoming connection
-    serverSocket.listen(1)
-
-    print("Server listening....")
-
-    #Waits for connection
-    while(1):
-        #Accept conncetion
-        connectionSocket, addr = serverSocket.accept()
-        print("Connected to client")
-
-        #Recieve whatever the newly connected client has to send
-        temporaryBuffer = ""
-        data = ""
-
-        length = connectionSocket.recv(1024)
-
-        while len(data) != 40:
-            #Receive whatever connected client sends
-            temporaryBuffer = connectionSocket.recv(40)
-
-            #OTher side unexpectedly closes
-            if not temporaryBuffer:
-                break
-            #Save data
-            data += temporaryBuffer
-
-    print(data)
-
-    #Close socket
-    connectionSocket.close()
-
+# Function:RetrFile
+# Date of code(Last updated): 11/25/17
+# Programmer: Brian Truong
+# Input: name, sock
+# Output: N/A
+# Description: Receives a response from the client and performs one of three actions:
+#   1) Sends a file from the server machine to the client
+#   2) Receives a file from the client machine
+#   3) Sends a list of items in the current directory of the server
 def RetrFile(name, sock):
-    filename = sock.recv(1024)
-    if os.path.isfile(filename):
-        fileInt = os.path.getsize(filename)
-        fileStr = 'EXISTS ' + str(fileInt)
-        byteName = str.encode(fileStr)
-        sock.send(byteName) #Wants to send a str, not bytes
-        #sock.send("Exists " + str(os.path.getsize(filename)))
-        userResponse = sock.recv(1024)
-        if userResponse[:2] == 'OK':    #Check first two bytes to see if it's okay
-            with open(filename, 'rb') as f:
-                bytesToSend = f.read(1024)  #Read 1024 bytes from the file
-                sock.send(bytesToSend)
-                while bytesToSend != "":    #If file exceeds 1024 bytes, grab another 1024 bytes
-                    bytesToSend = f.read(1024)
-                    sock.send(bytesToSend)
-    #else:   #If file doesn't exist
-    #    sock.send("ERR")
-    sock.close()
+    #Receives response from client; determines actions
+    userResponse = sock.recv(1024)
 
+    #If user responds with GET, server proceeds to send file to user
+    if(str(userResponse)[2:5] == 'GET'):
+        #Obtains file name
+        filename = sock.recv(1024)
+        #Confirms file existence
+        if os.path.isfile(filename):
+            fileInt = os.path.getsize(filename) #Gets file size
+            fileStr = 'EXISTS' + str(fileInt)   #Appends 'EXISTS' to string to send back to client for confirmation
+            byteName = str.encode(fileStr)  #Encodes string for socket transmission
+            sock.send(byteName) #Wants to send a str, not bytes
+            userResponse = sock.recv(1024)
+            if str(userResponse)[2:4] == 'OK':    #Check first two bytes to see if it's okay
+                print("OK received")
+                f= open(filename, 'rb') #Opens file for reading purposes
+                l = f.read(1024)    #Reads 1024 bytes from file
+                while(l):
+                    sock.send(l)    #Sends bytes to client
+                    l = f.read(1024)    #Reads next 1024 bytes
+        #testResponse = sock.recv(1024)
+        #if(str(testResponse)[2:] == 'CLOSE'):
+        #    systemMessage = 'OK'
+        #    systemMessage = str.encode(systemMessage)
+        #    sock.send(systemMessage)
+        sock.close()    #Closes socket after transmission
+    #If server receives PUT command, prepares to write data to server's directory
+    if(str(userResponse)[2:5] == 'PUT'):
+        filename = sock.recv(1024)  #Receives filename from client
+        f = open(filename, 'wb')    #Opens file to write to
+        data = sock.recv(1024)      #Receives data for file
+        totalRecv = len(data)       #Tallies amount of data received
+        while(data):
+            f.write(data)      #Writes data to file
+            data = sock.recv(1024)  #Retrieves more data from file
+        print("Download Complete")
+        sock.close()
+    #If server receives LS command, prepares to send list of files in the server directory
+    if(str(userResponse)[2:4] == 'LS'):
+        #Uses os.listdir() to check files in current dircetory
+        for line in os.listdir():
+            data = line #Assigns file name to data
+            print(data) #Prints files in directory; also prevents concatentation of filenames
+            data = str.encode(data)
+            sock.send(data) #Sends file name to client
+        sock.close()
+
+# Function: Main()
+# Date of code (Last updated): 11/25/17 
+# Prorgrammer: Brian Truong
+# Description: Initialize socket and thread for FTP server
+# Input: N/A
+# Output: N/A
 def Main():
-    host = 'localhost'
-    port = 5000
+    host = gethostbyname(socket.gethostname())  #Retrieves host IP; credit: https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    print(host)
+    port = int(input("Enter your port number: "))
+    #host = 'localhost'
+    #port = 5000
 
+    #Creates socket for server
     s = socket.socket()
     s.bind((host,port)) #Bind socket to host and port
 
     s.listen(5)
 
     print("Server Started")
+    #While server is active
     while True:
         c, addr = s.accept()
-        print("Client connected to IP :< " + str(addr) + ">")
-        t = threading.Thread(target = RetrFile, args=("retrThread", c))
-        t.start()
+        print("Client connected to IP :< " + str(addr) + ">")   #Confirms that connection is established
+        t = threading.Thread(target = RetrFile, args=("retrThread", c)) #Uses thread to run main function
+        t.start()   #Starts thread
     s.close()
 
 if __name__ == '__main__':
